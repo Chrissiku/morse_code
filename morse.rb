b1275e13a842ef10bc3c267532b12a0e414198e5
chrissiku <chrissiku5@gmail.com>
2022-08-17 19:03:41 +0200

    Implement changes

--- morse.rb
+++ morse.rb
@@ -1,71 +1,115 @@
+@dictionary = {
 
-    @dictionary = {
-    "a" => ".-",
-    "b" => "-...",
-    "c" => "-.-.",
-    "d" => "-..",
-    "e" => ".",
-    "f" => "..-.",
-    "g" => "--.",
-    "h" => "....",
-    "i" => "..",
-    "j" => ".---",
-    "k" => "-.-",
-    "l" => ".-..",
-    "m" => "--",
-    "n" => "-.",
-    "o" => "---",
-    "p" => ".--.",
-    "q" => "--.-",
-    "r" => ".-.",
-    "s" => "...",
-    "t" => "-",
-    "u" => "..-",
-    "v" => "...-",
-    "w" => ".--",
-    "x" => "-..-",
-    "y" => "-.--",
-    "z" => "--..",
-    "1" => ".----",
-    "2" => "..---",
-    "3" => "...--",
-    "4" => "....-",
-    "5" => ".....",
-    "6" => "-....",
-    "7" => "--...",
-    "8" => "---..",
-    "9" => "----.",  
-    "0" => "-----",
-    " " => " "
-    }
+  'a' => '.-',
+
+  'b' => '-...',
+
+  'c' => '-.-.',
+
+  'd' => '-..',
+
+  'e' => '.',
+
+  'f' => '..-.',
+
+  'g' => '--.',
+
+  'h' => '....',
+
+  'i' => '..',
+
+  'j' => '.---',
+
+  'k' => '-.-',
+
+  'l' => '.-..',
+
+  'm' => '--',
+
+  'n' => '-.',
+
+  'o' => '---',
+
+  'p' => '.--.',
+
+  'q' => '--.-',
+
+  'r' => '.-.',
+
+  's' => '...',
+
+  't' => '-',
+
+  'u' => '..-',
+
+  'v' => '...-',
+
+  'w' => '.--',
+
+  'x' => '-..-',
+
+  'y' => '-.--',
+
+  'z' => '--..',
+
+  '1' => '.----',
+
+  '2' => '..---',
+
+  '3' => '...--',
+
+  '4' => '....-',
+
+  '5' => '.....',
+
+  '6' => '-....',
+
+  '7' => '--...',
+
+  '8' => '---..',
+
+  '9' => '----.',
+
+  '0' => '-----',
+
+  ' ' => ' '
+
+}
 
 def decode_char(string)
-    return (@dictionary.key(string)).upcase()
+  @dictionary.key(string).upcase
 end
 
-puts decode_char('--')
+p decode_char('.-')
 
 def decode_word(str)
   splitStr = str.split
-  word = ""
+
+  word = ''
+
   splitStr.each do |char|
     word.concat(decode_char(char))
   end
-  return word.upcase()
+
+  word.upcase
 end
 
-puts decode_word("-- -.--")
+p decode_word('-- -.--')
 
 def decode_message(message)
-  splitMessage = message.split("   ")
-  result = ""
+  splitMessage = message.split('   ')
+
+  result = ''
+
   splitMessage.each do |word|
     result.concat(decode_word(word))
-    result.concat(" ") if word != splitMessage.last
+
+    result.concat(' ') if word != splitMessage.last
   end
-  return result.upcase()
+
+  result.upcase
 end
 
-puts decode_message("-- -.--   -. .- -- .")
-puts decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+p decode_message('-- -.--   -. .- -- .')
 
+p decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
